7480ff38aa9b8f395e299fa667aeb434127769fe
trungtinh998227 <trungtinh998227@gmail.com>
2018-10-16 11:15:29 +0700

    Create android quiz-system

---
+++ app/src/test/java/com/example/christian_dev/quiz_sytem/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.christian_dev.quiz_sytem;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
